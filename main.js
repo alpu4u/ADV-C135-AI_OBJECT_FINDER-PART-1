d6d55803da248cb053ac88a7eb182e57a3d2a341
alpu4u <86269479+alpu4u@users.noreply.github.com>
2023-01-03 16:51:47 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+objectName = "";
+statusModel = "";
+function preload()
+{
+    video = createCapture(VIDEO);
+    video.hide();
+}
 function setup()
 { 
     canvas= createCanvas(480, 380);
     canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Detecting Objects";
+    objectName = document.getElementById("objName").value;   
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded');
+    statusModel = true; 
 }
